21cf902acfa95fa340acf771449d21eaf9f7ce0b
ssjiyi <ssjiyi@163.com>
2016-09-09 16:27:42 +0800

    添加模块  消息中心模块

---
+++ dubbo-message/src/main/java/cn/zw/message/active/MessageAble.java
@@ -0,0 +1,14 @@
+package cn.zw.message.active;
+
+/**
+ * @description 消息接口 （用于监听）
+ * @auther 'Amos'
+ * @created 2016/9/8  18:18
+ */
+public interface MessageAble {
+
+    /**
+     *  监听消息
+     */
+    void  listener();
+}
