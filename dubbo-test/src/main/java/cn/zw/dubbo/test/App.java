580b61a2424de16fa2ad81aa2d27ea28b804181f
ssjiyi <ssjiyi@163.com>
2017-03-31 16:11:29 +0800

    add test module

---
+++ dubbo-test/src/main/java/cn/zw/dubbo/test/App.java
@@ -0,0 +1,20 @@
+package cn.zw.dubbo.test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static void main(String[] args) {
+
+        ConcurrentHashMap<String,String> map = new ConcurrentHashMap<>();
+
+        map.size();
+
+    }
+
+}
